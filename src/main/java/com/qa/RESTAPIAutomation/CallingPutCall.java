64bb878b1fd395ae95d9af362f32219a8bee6686
Dsilva-Rohan <rohanestar@gmail.com>
2020-10-13 23:06:39 +0530

    httpclient automation

--- src/main/java/com/qa/RESTAPIAutomation/CallingPutCall.java
+++ src/main/java/com/qa/RESTAPIAutomation/CallingPutCall.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
+
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
